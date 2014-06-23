1c4a0964476b674a2a104e5e4ff38c412bbed3e9
Peter Brady <ptb@lanl.gov>
2014-06-23 15:46:36 -0600

    added meta.yaml for conda builds

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
-setup(name="lrucache", version="0.1",
+setup(name="fastcache", version="0.1",
       packages = ["fastcache", "fastcache.tests"],
       ext_modules= [Extension("fastcache._lrucache",
                     ["src/_lrucache.c"],
