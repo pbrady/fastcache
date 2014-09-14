806e473e0aa7725c978a6413f04d5c52143e5759
Peter Brady <petertbrady@gmail.com>
2014-09-14 14:36:58 -0600

    changes for 1.0 release

--- fastcache/__init__.py
+++ fastcache/__init__.py
@@ -21,7 +21,7 @@ lru_cache  - python wrapper around clru_cache (slower)
            >>> <class 'function'>
 """
 
-__version__ = "0.4.3"
+__version__ = "1.0.0"
 
 
 from ._lrucache import clru_cache
